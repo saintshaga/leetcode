746bae7238f8aa81e575ce441bdcda8ac916ad4d
saintshaga <saintshaga@gmail.com>
2018-03-06 19:51:59 +0800

    update 33.search-in-rotated-sorted-array.java

--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -4,6 +4,34 @@ class Solution {
     	if(n == 0) {
     		return -1;
     	}
+    	int l=0,h=n-1;
+    	while(l < h) {
+    		int mid = (l+h)/2;
+    		if(nums[mid] == target) {
+    			return mid;
+    		}
+    		if(nums[mid] >= nums[l]) {
+    			if(target < nums[mid] && target >= nums[l]) {
+    				h = mid - 1;
+    			} else {
+    				l = mid + 1;
+    			}
+    		} else {
+    			if(target > nums[mid] && target < nums[l]) {
+    				l = mid + 1;
+    			} else {
+    				h = mid - 1;
+    			}
+    		}
+    	}
+    	return target == nums[l] ? l : -1;
+    }
+
+    private int searchMyVersion(int[] nums, int target) {
+    	int n = nums.length;
+    	if(n == 0) {
+    		return -1;
+    	}
     	if(nums[0] <= nums[n-1]) {
     		return binaraySearch(nums, 0, n, target);
     	}
