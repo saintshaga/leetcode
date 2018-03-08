b1fea1d508cb8d88fa642a57b867e5311ff3c867
saintshaga <saintshaga@gmail.com>
2018-03-08 21:02:21 +0800

    update 41.first-missing-positive.java

--- 41.first-missing-positive.java
+++ 41.first-missing-positive.java
@@ -1,17 +1,28 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        boolean[] records = new boolean[n+1];
         for(int i=0; i<n; i++) {
-        	if(nums[i] > 0 && nums[i] <= n+1) {
-        		records[nums[i] - 1] = true;
+        	while(nums[i] > 0 && nums[i] <= n && nums[i] != i+1) {
+        		if(!swap(nums, i, nums[i]-1)) {
+        			break;
+        		}
         	}
         }
-        for(int i=0; i<=n; i++) {
-        	if(!records[i]) {
+        for(int i=0; i<n; i++) {
+        	if(nums[i] != i+1) {
         		return i+1;
         	}
         }
-        return 1;
+        return n+1;
+    }
+
+    private boolean swap(int[] nums, int i, int j) {
+    	if(nums[i] == nums[j]) {
+    		return false;
+    	}
+    	int t = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = t;
+    	return true;
     }
 }
