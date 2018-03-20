c46a7a6c8dfbcbd9a89ec511898d0007433ff57f
saintshaga <saintshaga@gmail.com>
2018-03-20 10:08:58 +0800

    update 79.word-search.java

--- 79.word-search.java
+++ 79.word-search.java
@@ -3,9 +3,6 @@ class Solution {
         if(word.length() == 0) {
         	return true;
         }
-        if(board.length == 0 || board[0].length == 0) {
-        	return false;
-        }
         if(board.length == 1 && board[0].length == 1) {
         	return word.length() == 1 && board[0][0] == word.charAt(0);
         }
@@ -23,21 +20,24 @@ class Solution {
     	if(k >= word.length()) {
     		return true;
     	}
+    	if(i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+    		return false;
+    	}
     	if(board[i][j] != word.charAt(k)) {
     		return false;
     	}
     	char temp = board[i][j];
     	board[i][j] = 0;
-    	if(i-1 >= 0 && search(board, i-1, j, word, k+1)) {
+    	if(search(board, i-1, j, word, k+1)) {
     		return true;
     	}
-    	if(i+1 < board.length && search(board, i+1, j, word, k+1)) {
+    	if(search(board, i+1, j, word, k+1)) {
     		return true;
     	}
-    	if(j-1 >= 0 && search(board, i, j-1, word, k+1)) {
+    	if(search(board, i, j-1, word, k+1)) {
     		return true;
     	}
-    	if(j+1 < board[0].length && search(board, i, j+1, word, k+1)) {
+    	if(search(board, i, j+1, word, k+1)) {
     		return true;
     	}
     	board[i][j] = temp;
