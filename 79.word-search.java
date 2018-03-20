75a89e7e9747460fc3c6b451793f696bc4aedbe0
saintshaga <saintshaga@gmail.com>
2018-03-20 10:09:38 +0800

    update 79.word-search.java

--- 79.word-search.java
+++ 79.word-search.java
@@ -3,8 +3,8 @@ class Solution {
         if(word.length() == 0) {
         	return true;
         }
-        if(board.length == 1 && board[0].length == 1) {
-        	return word.length() == 1 && board[0][0] == word.charAt(0);
+        if(board.length == 0 || board[0].length == 0) {
+        	return false;
         }
         for(int i=0; i<board.length; i++) {
         	for(int j=0; j<board[0].length; j++) {
