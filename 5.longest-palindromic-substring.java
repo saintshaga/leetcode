4d9a5d007982987fd9cb7013a208ad698fad6784
saintshaga <saintshaga@gmail.com>
2018-03-21 16:35:56 +0800

    update 5.longest-palindromic-substring.java

--- 5.longest-palindromic-substring.java
+++ 5.longest-palindromic-substring.java
@@ -5,6 +5,33 @@ class Solution {
         }
         int n = s.length();
         char[] a = s.toCharArray();
+        boolean[][] c = new boolean[n][n];
+        int st = 0, maxLength = 0;
+        for(int delta=0; delta<=n-1; delta++) {
+        	for(int i=0; i <= n-1-delta; i++) {
+        		if(delta == 0) {
+        			c[i][i+delta] = true;
+        		} else if(delta == 1) {
+        			c[i][i+delta] = a[i] == a[i+delta] ? true : false;
+        		} else {
+        			c[i][i+delta] = a[i] == a[i+delta] ? c[i+1][i+delta-1] : false;
+        		}
+        		if(c[i][i+delta] && (delta+1) > maxLength) {
+        			st = i;
+        			maxLength = delta + 1;
+        		}
+        	}
+        }
+        return s.substring(st, st+maxLength);
+
+    }
+
+    private String longestPalindrome2(String s) {
+        if(s == null || s.length() <= 1) {
+        	return s;
+        }
+        int n = s.length();
+        char[] a = s.toCharArray();
         char[] b = new char[n];
         for(int i=0; i<n; i++) {
         	b[i] = a[n-1-i];
@@ -25,6 +52,5 @@ class Solution {
         }
         return s.substring(index+1-maxLength, index+1);
     }
-
 
 }
