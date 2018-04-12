5b40ad5c83ee909f9131724010b08a426993ba69
saintshaga <saintshaga@gmail.com>
2018-04-12 15:46:45 +0800

    update 9.palindrome-number.java

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -1,5 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
+    	if(x < 0 || x % 10 ==0 && x != 0) {
+    		return false;
+    	}
+    	int reverseNumber = 0;
+    	while(x > reverseNumber) {
+    		reverseNumber = reverseNumber * 10 + x%10;
+    		x /= 10;
+    	}
+
+    	return x == reverseNumber || x == reverseNumber/10;
+    }
+
+    public boolean isPalindrome1(int x) {
     	if(x < 0) {
     		return false;
     	}
