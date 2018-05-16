b88d6feabd5a47a35ba42813d81b3f3f8cbf5286
saintshaga <saintshaga@gmail.com>
2018-05-16 21:57:32 +0800

    update 112.path-sum.java

---
+++ 112.path-sum.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+        	return false;
+        }
+        if(root.left == null && root.right == null) {
+        	return sum == root.val;
+        } else if(root.left == null) {
+        	return hasPathSum(root.right, sum-root.val);
+        } else if(root.right == null) {
+        	return hasPathSum(root.left, sum-root.val);
+        } else {
+	        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+        }
+    }
+}
