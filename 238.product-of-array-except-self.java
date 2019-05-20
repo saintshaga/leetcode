bb4ecf60d6dc5b0aee5307c4efbfcaf02c538680
saintshaga <saintshaga@gmail.com>
2019-05-20 23:23:22 +0800

    update 238.product-of-array-except-self.java

--- 238.product-of-array-except-self.java
+++ 238.product-of-array-except-self.java
@@ -4,19 +4,15 @@ class Solution {
         	return nums;
         }
         int n = nums.length;
-        int[] left2Right = new int[n];
-        int[] right2Left = new int[n];
-        left2Right[0] = 1;
-        right2Left[n-1] = 1;
-        for(int i=1; i<n; i++) {
-        	left2Right[i] = left2Right[i-1] * nums[i-1];
-        }
-        for(int i=n-2; i>=0; i--) {
-        	right2Left[i] = right2Left[i+1] * nums[i+1];
-        }
         int[] result = new int[n];
-        for(int i=0; i<n; i++) {
-        	result[i] = left2Right[i] * right2Left[i];
+        result[0] = 1;
+        for(int i=1; i<n; i++) {
+        	result[i] = result[i-1] * nums[i-1];
+        }
+        int lastResult = 1;
+        for(int i=n-1; i>=0; i--) {
+        	result[i] *= lastResult;
+        	lastResult *= nums[i];
         }
         return result;
     }
