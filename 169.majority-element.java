98eb64a3ef71826b8ca22f3e640e233d426604fb
saintshaga <saintshaga@gmail.com>
2019-05-21 19:01:21 +0800

    update 169.majority-element.java

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -1,16 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        if(nums.length == 1) {
-        	return nums[0];
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++) {
-        	int count = map.containsKey(nums[i]) ? map.get(nums[i]) : 0;
-        	map.put(nums[i], ++count);
-        	if(count > nums.length/2) {
-        		return nums[i];
-        	}
-        }
-        return -1;
+    	int candidate = nums[0], count = 0;
+    	for(int i=0; i<nums.length; i++) {
+    		if(count == 0) {
+    			candidate = nums[i];
+    		}
+    		count += candidate == nums[i] ? 1 : -1;
+    	}
+    	return candidate;
     }
 }
