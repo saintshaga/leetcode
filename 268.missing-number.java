982c7779b8f7a6c4b33e93e600b7c13b86d49f4f
saintshaga <saintshaga@gmail.com>
2019-07-11 20:51:53 +0800

    update 268.missing-number.java

--- 268.missing-number.java
+++ 268.missing-number.java
@@ -1,22 +1,26 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        if(n == 0) {
+        if(nums == null || nums.length == 0) {
         	return -1;
         }
-        for(int i=0; i<n; i++) {
-        	while(nums[i] != n && i != nums[i]) {
+        int n = nums.length;
+        for(int i=0; i<nums.length; i++) {
+        	while(nums[i] != n && nums[i] != i) {
         		swap(nums, i, nums[i]);
         	}
         }
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<nums.length; i++) {
         	if(i != nums[i]) {
         		return i;
         	}
         }
         return n;
     }
+
     private void swap(int[] nums, int i, int j) {
+    	if(i == j) {
+    		return;
+    	}
     	int temp = nums[i];
     	nums[i] = nums[j];
     	nums[j] = temp;
