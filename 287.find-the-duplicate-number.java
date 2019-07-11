3231c7cdaab4d4efe4614179c712899d238f5af3
saintshaga <saintshaga@gmail.com>
2019-07-11 20:29:09 +0800

    update 287.find-the-duplicate-number.java

--- 287.find-the-duplicate-number.java
+++ 287.find-the-duplicate-number.java
@@ -1,20 +1,18 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        if(nums.length <= 1) {
-        	return -1;
-        }
-        int turtle = nums[0];
-        int rabit = nums[0];
-        do {
-        	turtle = nums[turtle];
-        	rabit = nums[nums[rabit]];
-        } while (turtle != rabit);
-        int num1 = nums[0];
-        int num2 = turtle;
-        while(num1 != num2) {
-        	num1 = nums[num1];
-        	num2 = nums[num2];
-        }
-        return num1;
+    	if(nums == null || nums.length <= 1) {
+    		return -1;
+    	}
+    	int rabbit = 0, turtle = 0;
+    	do {
+    		rabbit = nums[rabbit];
+    		turtle = nums[nums[turtle]];
+    	} while(nums[rabbit] != nums[turtle]);
+    	rabbit = 0;
+    	while(nums[rabbit] != nums[turtle]) {
+    		rabbit = nums[rabbit];
+    		turtle = nums[turtle];
+    	}
+    	return nums[rabbit];
     }
 }
