767d581b023f646b82328832e739a0f3ed55b657
saintshaga <saintshaga@gmail.com>
2019-07-11 21:03:14 +0800

    update 169.majority-element.java

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -1,12 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-    	int candidate = nums[0], count = 0;
-    	for(int i=0; i<nums.length; i++) {
-    		if(count == 0) {
-    			candidate = nums[i];
-    		}
-    		count += candidate == nums[i] ? 1 : -1;
-    	}
-    	return candidate;
+        if(nums == null || nums.length == 0) {
+        	return -1;
+        }
+        int candidate = nums[0];
+        int count = 0;
+        for(int i=0; i<nums.length; i++) {
+        	if(count == 0) {
+        		candidate = nums[i];
+        		count = 1;
+        	} else if(candidate == nums[i]) {
+        		count++;
+        	} else {
+        		count--;
+        	}
+        }
+        return candidate;
     }
 }
