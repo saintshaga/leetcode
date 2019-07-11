25aff2e27dbb00b21ac281be1eb844a007f8a260
saintshaga <saintshaga@gmail.com>
2019-07-11 20:39:00 +0800

    update 283.move-zeroes.java

--- 283.move-zeroes.java
+++ 283.move-zeroes.java
@@ -1,18 +1,24 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length <= 1) {
+        if(nums == null || nums.length <= 1) {
         	return;
         }
-        for(int p1 = 0, current = 0; current < nums.length; current++) {
-        	if(nums[current] != 0) {
-        		swap(nums, p1++, current);
+        int postionOfNonZero=0;
+        for(int i=0; i<nums.length; i++) {
+        	if(nums[i] != 0) {
+        		swap(nums, i, postionOfNonZero);
+        		postionOfNonZero++;
         	}
         }
+
     }
 
     private void swap(int[] nums, int i, int j) {
-    	int tmp = nums[i];
+    	if(i==j) {
+    		return;
+    	}
+    	int temp = nums[i];
     	nums[i] = nums[j];
-    	nums[j] = tmp;
+    	nums[j] = temp;
     }
 }
