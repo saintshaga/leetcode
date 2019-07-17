f91a87f5aab096aeb45042632b4dafbe0a565ff8
saintshaga <saintshaga@gmail.com>
2019-07-17 15:32:58 +0800

    update 32.longest-valid-parentheses.java

--- 32.longest-valid-parentheses.java
+++ 32.longest-valid-parentheses.java
@@ -1,23 +1,24 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        if(s == null || s.length() < 2) {
+        if(s == null || s.length() == 0) {
         	return 0;
         }
-        int n = s.length();
+        char[] chars = s.toCharArray();
+        int[] l = new int[chars.length];
+        l[0] = 0;
         int maxLength = 0;
-        int[] b = new int[n];
-        b[0] = 0;
-        for(int i=1; i<n; i++) {
-        	if(s.charAt(i) == '(') {
-        		b[i] = 0;
-        	} else if(s.charAt(i-1) == '(') {
-        		b[i] = (i >=2 ? b[i-2] : 0) + 2;
+        for(int i=1; i<chars.length; i++) {
+        	char c = chars[i];
+        	int pair = i-l[i-1]-1;
+        	if(c == ')' && pair >= 0 && chars[pair] == '(') {
+        		l[i] = l[i-1] + 2;
+        		if(pair-1 >= 0) {
+        			l[i] += l[pair-1];
+        		}
         	} else {
-        		b[i] = (i-b[i-1] >=1 && s.charAt(i-b[i-1]-1) == '(') ? b[i-1] + 2 + (i-b[i-1]>=2?b[i-b[i-1]-2]: 0): 0;
+        		l[i] = 0;
         	}
-        	if(b[i] > maxLength) {
-        		maxLength = b[i];
-        	}
+        	maxLength = Math.max(l[i], maxLength);
         }
         return maxLength;
     }
