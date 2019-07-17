203b7fa14ba5d8ba8296e4b9ac69cd1ad8577b6f
saintshaga <saintshaga@gmail.com>
2019-07-17 15:56:35 +0800

    update 152.maximum-product-subarray.java

--- 152.maximum-product-subarray.java
+++ 152.maximum-product-subarray.java
@@ -3,19 +3,21 @@ class Solution {
         if(nums == null || nums.length == 0) {
         	return 0;
         }
-        int n = nums.length;
-        int[] result = new int[n];
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<n; i++) {
-        	result[i] = nums[i];
-        	max = Math.max(max, result[i]);
-        } 
-        for(int distance = 1; distance <= n-1; distance++) {
-        	for(int i=0; i<n-distance; i++) {
-        		result[i] *= nums[i+distance];
-        		max = Math.max(max, result[i]);
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+        max[0] = nums[0];
+        min[0] = nums[0];
+        int result = max[0];
+        for(int i=1; i<nums.length; i++) {
+        	if(nums[i] >= 0) {
+        		max[i] = Math.max(nums[i], nums[i]*max[i-1]);
+        		min[i] = Math.min(nums[i], nums[i]*min[i-1]);
+        	} else {
+        		max[i] = Math.max(nums[i], nums[i]*min[i-1]);
+        		min[i] = Math.min(nums[i], nums[i]*max[i-1]);
         	}
+        	result = Math.max(result, max[i]);
         }
-        return max;
+        return result;
     }
 }
