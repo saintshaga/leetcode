66e11faca3b91a9c4cb37f7cefc7751e0125952c
saintshaga <saintshaga@gmail.com>
2019-07-24 12:17:57 +0800

    update 72.edit-distance.java

--- 72.edit-distance.java
+++ 72.edit-distance.java
@@ -1,25 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        if(word1 == null) {
-        	word1 = "";
-        }
-        if(word2 == null) {
-        	word2 = "";
+        if(word1 == null || word2 == null) {
+        	return -1;
         }
         int n1 = word1.length(), n2 = word2.length();
         int[][] c = new int[n1+1][n2+1];
-        for(int i=0; i<=n1; i++) {
+        c[0][0] = 0;
+        for(int i=1; i<=n1; i++) {
         	c[i][0] = i;
         }
-        for(int i=0; i<=n2; i++) {
+        for(int i=1; i<=n2; i++) {
         	c[0][i] = i;
         }
         for(int i=1; i<=n1; i++) {
         	for(int j=1; j<=n2; j++) {
-        		if(word1.charAt(i-1) == word2.charAt(j-1)) {
+        		char c1 = word1.charAt(i-1);
+        		char c2 = word2.charAt(j-1);
+        		if(c1 == c2) {
         			c[i][j] = c[i-1][j-1];
         		} else {
-        			c[i][j] = Math.min(Math.min(c[i-1][j], c[i][j-1]),c[i-1][j-1]) + 1;
+        			c[i][j] = Math.min(Math.min(c[i-1][j-1], c[i-1][j]), c[i][j-1]) + 1;
         		}
         	}
         }
