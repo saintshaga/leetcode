1e52bb9890ef49cd283c4e106fc4940711504d88
saintshaga <saintshaga@gmail.com>
2019-07-31 02:30:30 +0800

    update 42.trapping-rain-water.java

--- 42.trapping-rain-water.java
+++ 42.trapping-rain-water.java
@@ -1,22 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        if(height.length <= 2) {
+        if(height == null || height.length <= 2) {
         	return 0;
         }
-        int result = 0;
         int n = height.length;
-        int[] maxLeft = new int[n];
-        maxLeft[0] = height[0];
-        for(int i=1; i<n; i++) {
-        	maxLeft[i] = Math.max(maxLeft[i-1], height[i]);
-        }
-        int[] maxRight = new int[n];
-        maxRight[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--) {
-        	maxRight[i] = Math.max(maxRight[i+1], height[i]);
-        }
+        int left[] = new int[n];
+        int right[] = new int[n];
+        int max = height[0];
         for(int i=0; i<n; i++) {
-        	result += Math.min(maxLeft[i], maxRight[i]) - height[i];
+        	max = Math.max(height[i], max);
+        	left[i] = max;
+        }
+        max=height[n-1];
+        for(int i=n-1; i>=0; i--) {
+        	max = Math.max(height[i], max);
+        	right[i] = max;
+        }
+        int result = 0;
+        for(int i=0; i<n; i++) {
+        	result += Math.min(left[i], right[i]) - height[i];
         }
         return result;
     }
