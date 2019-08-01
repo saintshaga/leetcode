8527356a44aff0a36d651772651c6dccc4980831
saintshaga <saintshaga@gmail.com>
2019-08-01 23:41:33 +0800

    update 81.search-in-rotated-sorted-array-ii.java

--- 81.search-in-rotated-sorted-array-ii.java
+++ 81.search-in-rotated-sorted-array-ii.java
@@ -1,29 +1,35 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        if(nums.length == 0) {
+        if(nums == null || nums.length == 0) {
         	return false;
         }
-        int l = 0, h = nums.length-1;
+        int l=0, h=nums.length-1;
         while(l <= h) {
         	int mid = (l+h)/2;
-        	if(target == nums[mid] || target == nums[l]) {
+        	if(nums[mid] == target) {
         		return true;
-        	} else if(nums[mid] > nums[l]) {
-        		if(target > nums[l] && target < nums[mid]) {
-        			h = mid - 1;
+        	}
+        	if(nums[mid] > nums[0]) {
+        		if(target < nums[mid] && target >= nums[0]) {
+        			h = mid -1;
         		} else {
         			l = mid + 1;
         		}
-        	} else if(nums[mid] < nums[l]) {
-        		if(target < nums[l] && target > nums[mid]) {
-        			l = mid+1;
+        	} else if(nums[mid] < nums[0]) {
+        		if(target > nums[mid] && target < nums[0]) {
+        			l = mid + 1;
         		} else {
         			h = mid - 1;
         		}
         	} else {
-        		l++;
+        		if(nums[l] == target) {
+        			return true;
+        		} else {
+        			l++;
+        		}
         	}
         }
         return false;
+
     }
 }
