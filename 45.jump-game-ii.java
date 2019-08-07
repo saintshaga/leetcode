a440d4ee24459f8b0168fcdcfd895467687a9d6d
saintshaga <saintshaga@gmail.com>
2019-08-07 19:10:35 +0800

    update 45.jump-game-ii.java

--- 45.jump-game-ii.java
+++ 45.jump-game-ii.java
@@ -1,40 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        int n= nums.length;
-        if(n <= 1) {
+        if(nums == null || nums.length <= 1) {
         	return 0;
         }
-        int step = 0, currentMax = -1, nextMax = 0;
-        for(int i=0; i<n; i++) {
-        	if(i > currentMax) {
-        		step++;
-        		currentMax = nextMax;
+        int[] steps = new int[nums.length];
+        steps[0] = 0;
+        for(int i=0; i<nums.length; i++) {
+        	if(i+nums[i] >= nums.length-1) {
+        		return steps[i] + 1;
         	}
-        	if(nums[i] + i >= n-1) {
-        		return step;
+        	for(int j=i+nums[i]; (j>i && steps[j] == 0); j--) {
+        		steps[j] = steps[i] + 1;
         	}
-        	nextMax = Math.max(nextMax,nums[i] + i);
         }
-        return step;
-    }
-
-    private int jumpOn2(int[] nums) {
-        int n= nums.length;
-        if(n <= 1) {
-        	return 0;
-        }
-        int[] c = new int[n];
-        c[n-1] = 0;
-        for(int i=n-2; i>=0; i--) {
-        	c[i] = n-1;
-        	int steps = Math.min(nums[i], n-1-i);
-        	for(int j=1; j<=steps; j++) {
-        		if(c[i+j] < c[i]) {
-        			c[i] = c[i+j];
-        		}
-        	}
-        	c[i] += 1;
-        }
-        return c[0] == n ? 0 : c[0];
+        return steps[nums.length-1];
     }
 }
